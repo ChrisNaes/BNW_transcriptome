471521bacf6e6edf9fc15108bf857a2bae69b2b6
Christina Næsborg-Nielsen <83581714+ChrisNaes@users.noreply.github.com>
2026-08-28 14:15:07 -0400

    Remove job configuration from TrimGalore script
    Removed SLURM job configuration and input/output directory variables.

--- scripts/03_TrimGalore_single.sh
+++ scripts/03_TrimGalore_single.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=TrimGalore_single             # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --mem=20gb                    # Job memory request
-#SBATCH --time=100:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/home/cn68176/Log/%x.%j.out            # Standard output log
-#SBATCH --error=/home/cn68176/Log/%x.%j.err             # Standard error log
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=cn68176@uga.edu   # Where to send mail
-
-INDIR="/scratch/cn68176/RNAseq_BNW/Raw_reads"
-OUTDIR="/scratch/cn68176/RNAseq_BNW/Trimmed_reads"
-
 ml Trim_Galore/0.6.7-GCCcore-11.2.0
 ml cutadapt/4.5-GCCcore-11.3.0
 
