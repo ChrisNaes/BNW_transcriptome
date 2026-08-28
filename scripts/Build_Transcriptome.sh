9655df3b2355da3ce6c50eeb7ae4a07e8dae3f0f
Christina Næsborg-Nielsen <83581714+ChrisNaes@users.noreply.github.com>
2026-08-28 14:28:46 -0400

    Modify transcriptome build script for output and processing
    Updated output directory name and added steps to convert SAM to BAM files, merge BAM files by tissue type, and sort and index the BAM files.

--- scripts/Build_Transcriptome.sh
+++ scripts/Build_Transcriptome.sh
@@ -1,6 +1,6 @@
 # Define directories and reference genome
 INDIR="Genome/Tissues/Trimmed"
-OUTDIR="Genome/Tissues/Transcriptome1"
+OUTDIR="Genome/Tissues/Transcriptome"
 REFDIR="Genome" # Adjust depending on the genome needed
 REF="vu-2k.fasta" # Adjust depending on the genome needed
 
@@ -32,3 +32,38 @@ export INDIR OUTDIR REFDIR REF THREADS
 # Run HiSat2 alignment in parallel using GNU parallel
 ls ${INDIR}/*_R1_val_1.fq.gz | sed 's/_R1_val_1.fq.gz//' | xargs -I{} -P 4 bash -c 'align_reads "$@"' _ {}
 
+-----------------------------------------------------------------
+
+ml SAMtools
+
+## We first use samtools to create sorted and indexed BAM files for our samples
+for FILE in $OUTDIR/*.sam; do
+       #Extract the base filename
+       base_name=$(basename "$FILE" .sam)
+
+       # Convert SAM to BAM files
+       samtools view -bS "$OUTDIR/${base_name}.sam" > "$OUTDIR/${base_name}.bam"
+
+done
+
+cd $OUTDIR
+# Now merge the tissue specific bam files into one per tissue
+samtools merge -o Liver.bam Liver_S19_L003.bam Liver_S25_L004.bam
+samtools merge -o Lung.bam Lung_S22_L003.bam Lung_S28_L004.bam
+samtools merge -o Lymph-Node.bam Lymph-Node_S18_L003.bam Lymph-Node_S24_L004.bam
+samtools merge -o Skeletal-Muscle.bam Skeletal-Muscle_S20_L003.bam Skeletal-Muscle_S26_L004.bam 
+samtools merge -o Skin.bam Skin_S17_L003.bam Skin_S23_L004.bam
+samtools merge -o Thyroid.bam Thyroid_S21_L003.bam Thyroid_S27_L004.bam 
+
+for FILE in $OUTDIR/*.bam; do
+#       #Extract the base filename
+       base_name=$(basename "$FILE" .bam)
+#       # Sort BAM files
+       samtools sort -@ 8 "$OUTDIR/${base_name}.bam" -o "$OUTDIR/${base_name}_sorted.bam"
+
+#       # Index the sorted BAM files
+       samtools index "$OUTDIR/${base_name}_sorted.bam"
+
+#       # Print the information for the sample that completes then remove the temporary files
+       echo ${base_name}.bam
+done
