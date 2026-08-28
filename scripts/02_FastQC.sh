ca63a320291dd05a6bba6ed1d3cacf94cf4c20d5
Christina Næsborg-Nielsen <83581714+ChrisNaes@users.noreply.github.com>
2026-08-28 14:14:09 -0400

    Refactor FastQC script by removing job scheduler directives
    Removed SLURM job configuration and set input/output directories.

--- scripts/02_FastQC.sh
+++ scripts/02_FastQC.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=Quality_check      # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --mem=20gb                     # Job memory request
-#SBATCH --time=24:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/home/cn68176/Log/%x.%j.out            # Standard output log
-#SBATCH --error=/home/cn68176/Log%x.%j.err             # Standard error log
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=cn68176@uga.edu   # Where to send mail  
-
-INDIR="/scratch/cn68176/RNAseq_BNW/Genome/Tissues"
-OUTDIR="/scratch/cn68176/RNAseq_BNW/QC"
-
 cd $INDIR
 
 ml FastQC/0.11.9-Java-11
