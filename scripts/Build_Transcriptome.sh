f17f998339f72889bd67db36eddd15114c305c82
Christina Næsborg-Nielsen <83581714+ChrisNaes@users.noreply.github.com>
2026-08-28 14:26:08 -0400

    Refactor Build_Transcriptome.sh script
    Removed SLURM job configuration and updated directory paths.

--- scripts/Build_Transcriptome.sh
+++ scripts/Build_Transcriptome.sh
@@ -1,28 +1,15 @@
-#!/bin/bash
-#SBATCH --job-name=Align_Hisat2       # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --mem=100gb                     # Job memory request
-#SBATCH --time=100:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/home/cn68176/Log/%x.%j.out            # Standard output log
-#SBATCH --error=/home/cn68176/Log/%x.%j.err             # Standard error log
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=cn68176@uga.edu   # Where to send mail
-
-#!/bin/bash
-
 # Define directories and reference genome
-INDIR="/scratch/cn68176/RNAseq_BNW/Genome/Tissues/Trimmed"
-OUTDIR="/scratch/cn68176/RNAseq_BNW/Genome/Tissues/Transcriptome1"
-REFDIR="/scratch/cn68176/RNAseq_BNW/Genome" # Adjust depending on the genome needed
+INDIR="Genome/Tissues/Trimmed"
+OUTDIR="Genome/Tissues/Transcriptome1"
+REFDIR="Genome" # Adjust depending on the genome needed
 REF="vu-2k.fasta" # Adjust depending on the genome needed
 
 # Load necessary modules
 ml Miniforge3/24.7.1-0
 ml Miniconda3
-source activate /home/cn68176/hisat2_conda
+source activate hisat2_conda
 
-#hisat2-build $REFDIR/$REF $REFDIR/$REF
+hisat2-build $REFDIR/$REF $REFDIR/$REF
 
 # Set number of threads (adjust as needed)
 THREADS=8  # Change this based on available CPU resources
